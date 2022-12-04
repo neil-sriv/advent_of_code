88cc77704ad922c201668f005ce9ad3cfcaf3838
Neil Srivastava <neil.srivastava1@gmail.com>
2022-12-04 02:19:37 -0800

    organization

--- 2022/day5.py
+++ 2022/day5.py
@@ -2,4 +2,6 @@ from pathlib import Path
 
 dir_path = Path(__file__).resolve().parent
 with open(f"{dir_path}/day5_input.txt") as file:
-    data = file.read().splitlines()
+	data = file.read().splitlines()
+
+
