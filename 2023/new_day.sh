0a3e4650e880d60041beaf7694227ff14f41572c
Neil Srivastava <neil.srivastava@benchling.com>
2023-12-02 02:21:15 -0800

    rework automation

--- 2023/new_day.sh
+++ 2023/new_day.sh
@@ -5,7 +5,11 @@ url=https://adventofcode.com/2023/day/$day
 
 # curl --ouput $day_input.txt $url
 curl $url/input --cookie session=53616c7465645f5f8596b60abec4a35d4f4361fcb20077c57633bd50392f363364432fca471531fb26ca79f1e8a5d6a95cc0354700788d12f4dd792327bcf060 --output day${day}_input.txt
-
-echo -e "from pathlib import Path\n\ndir_path = Path(__file__).resolve().parent\nwith open(f\"{dir_path}/day${day}_input.txt\") as file:\n\tdata = file.read().splitlines()\n\n" > day$day.py
+curl $url --cookie session=53616c7465645f5f8596b60abec4a35d4f4361fcb20077c57633bd50392f363364432fca471531fb26ca79f1e8a5d6a95cc0354700788d12f4dd792327bcf060 | lynx -stdin -dump > day${day}_question.txt
+python get_question.py $day
+# echo -e "from pathlib import Path\n\ndir_path = Path(__file__).resolve().parent\nwith open(f\"{dir_path}/day${day}_input.txt\") as file:\n\tdata = file.read().splitlines()\n\n" > day$day.py
+echo "\"\"\"\n" | cat - day${day}_question.txt > day$day.py
+echo "\n\"\"\"\n" >> day$day.py
+cat template.txt >> day$day.py
 
 open -a Firefox -g $url
